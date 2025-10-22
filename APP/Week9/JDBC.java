30c4af0c34e10f7c18f39bb8312edc8dcf129e02
Harshal Chaprot <harshalchaprot1@gmail.com>
2025-10-23 01:59:31 +0530

    Add files via upload

---
+++ APP/Week9/JDBC.java
@@ -0,0 +1,7 @@
+spring.application.name=HappyPaws
+
+spring.datasource.url=jdbc:postgresql://localhost:5432/happypaws_db
+spring.datasource.username=postgres
+spring.datasource.password=harshal
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
